460ee0bbdc0ba88bf2d69749d86003adddec2032
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-05-19 17:55:21 -0300

    Update Aboutme.tsx

--- src/screens/Aboutme.tsx
+++ src/screens/Aboutme.tsx
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Aboutme() {
     return (
         <View style={styles.container}>
-            <Text>Aboutme</Text>
+            <Text>Ola, sou Wectornanime, tenho 21 anos de idade, sou técnico em redes de computadores pela ETE - Lucilo Ávila Pessoa, e tualmente estudo Análise e Desenvolvimento de Sistemas na Faculdade Senac PE, pelo programa Embarque Digital. Estou sempre buscando praticar e melhorar minhas habilidadese, além de conhecer novas tecnologias. Atualmente, trabalho como estagiário do Senac no Cartão do Empresário, onde colaboro como desenvolvedor backend, já atuei em projetos com flutter e angular, que me permitiram ampliar minha experiencia pratica, alem de conhecer novas tecnologias.
+            </Text>
         </View>
     );
 }
@@ -14,5 +15,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 15,
     },
 });
