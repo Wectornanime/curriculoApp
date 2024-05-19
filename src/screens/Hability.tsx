a228e2db85aa26be65c047e8d1a94a3fa584c902
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-05-19 18:15:23 -0300

    Update Hability.tsx

--- src/screens/Hability.tsx
+++ src/screens/Hability.tsx
@@ -3,7 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Hability() {
     return (
         <View style={styles.container}>
-            <Text>Hability</Text>
+            <View>
+                <Text>- Bancos de dados relacionais, MySQL;</Text>
+                <Text>- Tecnologias frontend, HTML, CSS, Javascript e Typescript;</Text>
+                <Text>- Frameworks modernos, React e Angular;</Text>
+                <Text>- Desenvolvimento de API restful em Python com Flask, criação e consumo;</Text>
+                <Text>- Desenvolvimento de API restful em NodeJs com NestJs, criação e consumo;</Text>
+                <Text>- Comunicação;</Text>
+                <Text>- Curiosidade;</Text>
+                <Text>- Pensamento crítico.</Text>
+            </View>
+            <Text style={styles.title2}>Linguagens de programação</Text>
+            <View>
+                <Text>- Python3</Text>
+                <Text>- JavaScript</Text>
+                <Text>- TypeScript</Text>
+            </View>
         </View>
     );
 }
@@ -12,7 +27,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 25,
+        paddingTop: 20,
+    },
+    
+    title2: {
+        fontSize: 24,
+        textAlign: 'center',
     },
 });
