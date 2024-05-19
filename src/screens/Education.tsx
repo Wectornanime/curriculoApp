2a5aa6f6d4ce7f39392a03c8e2d21c0ae4b35919
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-05-19 18:29:28 -0300

    Update Education.tsx

--- src/screens/Education.tsx
+++ src/screens/Education.tsx
@@ -3,7 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Education() {
     return (
         <View style={styles.container}>
-            <Text>Education</Text>
+            <Text style={styles.title2}>Educação</Text>
+            <Text style={styles.title3}>Faculdade Senac PE</Text>
+            <Text>Graduação - Análise e Desenvolvimento de Sistemas (manhã) | 2023-2025</Text>
+            <Text style={styles.title3}>ETE - Professor Lucilo Ávila Pessoa</Text>
+            <Text>Ensino Médio Téc. em Redes de Comp. | 2018 - 2020</Text>
+
+            <Text style={styles.title2}>Idiomas</Text>
+            <Text>- Português Nativo</Text>
+            <Text>- Inglês Básico</Text>
+            <Text>- Espanhol Básico</Text>
+
+            <Text style={styles.title2}>Formação Complementar</Text>
+            <Text>- Pacote Office</Text>
+            <Text>- Introdução à Amazon AWS</Text>
+            <Text>- Introdução à Microsoft Azure</Text>
+
         </View>
     );
 }
@@ -12,7 +27,17 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 25,
+        paddingTop: 20,
+    },
+    
+    title2: {
+        fontSize: 24,
+        textAlign: 'center',
+    },
+
+    title3: {
+        fontSize: 18,
+        fontStyle: 'italic',
     },
 });
